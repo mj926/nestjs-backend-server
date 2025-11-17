a7855e58a5ff6e67e2db068da830b370c1b6c467
dlalswjd95-mis <dldlalswjd95@gmail.com>
2025-11-17 21:55:13 +0900

    feat: 설정값 추가 및 기타 수정사항 반영

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // ✨ CORS 설정 추가 ✨
+  app.enableCors({
+    origin: 'http://localhost:5173', // Vue 개발 서버 주소
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  // ✨ 추가 끝 ✨
+
+  await app.listen(3000); // NestJS는 기본 포트 3000번 사용
+
+  // await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
